e26b09143c41fd6c6dd69f851619b7ed8f57104d
Sookim18 <scstnghks@naver.com>
2022-02-19 16:02:09 +0900

    🎉 [Start] : xcode 프로젝트 추가

---
+++ NFT-Helper/NFT-Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NFT-Helper
+//
+//  Created by sookim on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+

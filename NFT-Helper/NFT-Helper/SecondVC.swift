58acf47c756725d3f7dd165435c853a2184407c6
sookim-1 <scstnghks@gmail.com>
2022-02-26 19:35:53 +0900

    💄 [UI] : 탭바컨트롤러 추가 - 메인화면으로 설정

---
+++ NFT-Helper/NFT-Helper/SecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  SecondVC.swift
+//  NFT-Helper
+//
+//  Created by sookim on 2022/02/26.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}

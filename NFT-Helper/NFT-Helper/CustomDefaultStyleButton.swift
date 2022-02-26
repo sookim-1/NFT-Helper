ce4b565c2f821fa5ecde347adb2ccf2409d8bc8e
Sookim18 <scstnghks@naver.com>
2022-02-26 19:46:14 +0900

    💄 [UI] : 기본스타일 커스텀 버튼, 텍스트필드 추가

---
+++ NFT-Helper/NFT-Helper/CustomDefaultStyleButton.swift
@@ -0,0 +1,35 @@
+//
+//  CustomDefaultStyleButton.swift
+//  NFT-Helper
+//
+//  Created by sookim on 2022/02/26.
+//
+
+import UIKit
+
+final class CustomDefaultStyleButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}

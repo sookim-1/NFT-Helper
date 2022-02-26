12d93aa0b5a8808de4e57f5ca556f799872d093d
sookim-1 <scstnghks@gmail.com>
2022-02-26 20:35:19 +0900

    💄 [UI] : 기본스타일 제목, 본문레이블 추가

---
+++ NFT-Helper/NFT-Helper/CustomDefaultStyleBodyLabel.swift
@@ -0,0 +1,37 @@
+//
+//  CustomDefaultStyleBodyLabel.swift
+//  NFT-Helper
+//
+//  Created by sookim on 2022/02/26.
+//
+
+import UIKit
+
+final class CustomDefaultStyleBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+    }
+    
+}

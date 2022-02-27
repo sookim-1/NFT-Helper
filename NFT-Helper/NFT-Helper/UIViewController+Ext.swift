86e7d11bbc2affde89d9a89b6e88d277abc9ce05
sookim-1 <scstnghks@gmail.com>
2022-02-27 12:45:54 +0900

    ✨ [Feat] : 알럿창 띄우는 메서드 추가

--- NFT-Helper/NFT-Helper/UIViewController+Ext.swift
+++ NFT-Helper/NFT-Helper/UIViewController+Ext.swift
@@ -13,4 +13,14 @@ extension UIViewController {
     func centerToastMessage(text: String) {
         self.view.makeToast(text, point: self.view.center, title: nil, image: nil, completion: nil)
     }
+    
+    func presentDefaultStyleAlertVC(title: String, body: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = CustomDefaultStyleAlertVC(title: title, body: body, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            
+            self.present(alertVC, animated: true)
+        }
+    }
 }

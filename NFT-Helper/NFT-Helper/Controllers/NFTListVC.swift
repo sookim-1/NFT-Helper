d02063387ad5e3789d91a4dc3430526df157843d
sookim-1 <scstnghks@gmail.com>
2022-02-27 14:13:07 +0900

    💄 [UI] : 컬렉션뷰 추가 - 3열 그리드형식

--- NFT-Helper/NFT-Helper/Controllers/NFTListVC.swift
+++ NFT-Helper/NFT-Helper/Controllers/NFTListVC.swift
@@ -12,12 +12,14 @@ import SnapKit
 final class NFTListVC: UIViewController {
 
     var walletAddress: String!
+    private var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "NFT 목록"
         configureNavigationBar()
+        configureCollectionView()
         getAddressCollections()
     }
     
@@ -29,6 +31,29 @@ final class NFTListVC: UIViewController {
 
     private func configureNavigationBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    // MARK: 컬렉션뷰 초기화
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(AssetsCollectionViewCell.self, forCellWithReuseIdentifier: AssetsCollectionViewCell.reuseID)
+    }
+    
+    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+
+        return flowLayout
     }
     
     // MARK: 네트워킹

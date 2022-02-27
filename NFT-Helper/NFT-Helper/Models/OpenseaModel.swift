081f698d1d48c70154549c1b6383c10bb31a5e08
sookim-1 <scstnghks@gmail.com>
2022-02-27 13:11:32 +0900

    ✨ [Feat] : 오픈씨 API 통신모델 추가

---
+++ NFT-Helper/NFT-Helper/Models/OpenseaModel.swift
@@ -0,0 +1,28 @@
+//
+//  OpenseaModel.swift
+//  NFT-Helper
+//
+//  Created by sookim on 2022/02/27.
+//
+
+import Foundation
+
+struct AddressCollectionModel: Codable {
+    let name: String
+    let externalURL: String
+    let imageURL: String
+    let slug: String
+    let ownedAssetCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case externalURL = "external_url"
+        case imageURL = "image_url"
+        case slug
+        case ownedAssetCount = "owned_asset_count"
+    }
+}
+
+struct SingleAssetStatsModel: Codable {
+    let stats: [String: Double]
+}

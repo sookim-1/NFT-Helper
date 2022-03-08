862c469e906223fa2e54d3e868d3ce7af0eb2520
Sookim18 <scstnghks@naver.com>
2022-03-08 11:09:56 +0900

    ✨ [Feat] : NFTListVC - 검색 및 스크롤 감지 기능 구현

--- NFT-Helper/NFT-Helper/Controllers/NFTListVC.swift
+++ NFT-Helper/NFT-Helper/Controllers/NFTListVC.swift
@@ -16,17 +16,27 @@ final class NFTListVC: UIViewController {
     }
     
     var walletAddress: String!
-    private var collectionView: UICollectionView!
+    
     private var addressCollectionModels: [AddressCollectionModel] = []
+    private var filterdAddressCollectionModels: [AddressCollectionModel] = []
+    
+    private var offset: Int = 0
+    private var limit: Int = 10
+    private var hasMoreModels = true
+    private var isSearching = false
+    
+    private var collectionView: UICollectionView!
     private var dataSource: UICollectionViewDiffableDataSource<Section, AddressCollectionModel>!
     
+    // MARK: 뷰컨트롤러 생명주기
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "NFT 목록"
         configureNavigationBar()
+        configureSearchController()
         configureCollectionView()
-        getAddressCollections()
+        getAddressCollections(offset: offset, limit: limit)
         configureDataSource()
     }
     
@@ -36,8 +46,19 @@ final class NFTListVC: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
+    
+    // MARK: UIKit
     private func configureNavigationBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "이름을 검색하세요"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
     }
     
     // MARK: 컬렉션뷰 초기화
@@ -45,6 +66,7 @@ final class NFTListVC: UIViewController {
     private func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(AssetsCollectionViewCell.self, forCellWithReuseIdentifier: AssetsCollectionViewCell.reuseID)
     }
@@ -74,11 +96,11 @@ final class NFTListVC: UIViewController {
     }
     
     
-    private func updateData() {
+    private func updateData(on models: [AddressCollectionModel]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, AddressCollectionModel>()
         
         snapshot.appendSections([.main])
-        snapshot.appendItems(addressCollectionModels)
+        snapshot.appendItems(models)
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true)
         }
@@ -86,18 +108,55 @@ final class NFTListVC: UIViewController {
     
     // MARK: 네트워킹
     
-    private func getAddressCollections() {
-        NetworkManager.shared.getCollections(url: Endpoint.collections(assetOwner: walletAddress, limit: 100).url) { [weak self] result in
+    private func getAddressCollections(offset: Int, limit: Int) {
+        showLoadingView()
+        NetworkManager.shared.getCollections(url: Endpoint.collections(assetOwner: walletAddress, offset: offset, limit: limit).url) { [weak self] result in
             guard let self = self else { return }
             
+            self.dismissLoadingView()
             switch result {
             case .success(let value):
-                self.addressCollectionModels = value
-                self.updateData()
+                if value.count < 10 { self.hasMoreModels = false }
+                self.addressCollectionModels.append(contentsOf: value)
+                
+                if self.addressCollectionModels.isEmpty {
+                    print("자산 모델이 없습니다.")
+                    return
+                }
+                self.updateData(on: value)
             case .failure(let error):
                 self.presentDefaultStyleAlertVC(title: "에러", body: error.rawValue, buttonTitle: "확인")
                 self.navigationController?.popViewController(animated: true)
             }
         }
     }
+}
+
+extension NFTListVC: UICollectionViewDelegate {
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreModels else { return }
+            limit += 10
+            getAddressCollections(offset: offset, limit: limit)
+        }
+    }
+}
+
+extension NFTListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
+        filterdAddressCollectionModels = addressCollectionModels.filter { $0.name.lowercased().contains(filter.lowercased()) }
+        updateData(on: filterdAddressCollectionModels)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+        updateData(on: addressCollectionModels)
+    }
+
 }
--- NFT-Helper/NFT-Helper/NetworkService/EndPoint.swift
+++ NFT-Helper/NFT-Helper/NetworkService/EndPoint.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 enum Endpoint {
-    case collections(assetOwner: String, limit: Int)
+    case collections(assetOwner: String, offset: Int, limit: Int)
     case collectionStats(collectionSlug: String)
 }
 
 extension Endpoint {
     var url: URL {
         switch self {
-        case .collections(let assetOwner, let limit):
-            return .makeForEndpoint("collections?asset_owner=\(assetOwner)&offset=0&limit=\(limit)")
+        case .collections(let assetOwner, let offset, let limit):
+            return .makeForEndpoint("collections?asset_owner=\(assetOwner)&offset=\(offset)&limit=\(limit)")
         case .collectionStats(let slug):
             return .makeForEndpoint("collection/\(slug)/stats")
         }

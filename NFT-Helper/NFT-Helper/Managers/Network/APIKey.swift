ef5ea570a2fd76168b94f8e6e8d34b77bc61158e
sookim-1 <scstnghks@naver.com>
2022-05-03 21:13:20 +0900

    ♻️ [Refactor] : 파일 폴더별 분리

---
+++ NFT-Helper/NFT-Helper/Managers/Network/APIKey.swift
@@ -0,0 +1,14 @@
+//
+//  APIKey.swift
+//  NFT-Helper
+//
+//  Created by sookim on 2022/04/26.
+//
+
+import Foundation
+
+enum PaPagoAPIKey {
+    static let ClientID: String = "07aUlO6yHGuNrfJCpVjE"
+    static let ClientSecret: String = "F5uD6G5C0E"
+    static let papagoURLString: String = "https://openapi.naver.com/v1/papago/n2mt"
+}
